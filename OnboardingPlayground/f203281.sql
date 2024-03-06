03bd6d6da8a1fd592158429e7f42531e2d8ac140
Javier Meza <javier.meza@atlas.com.py>
2024-03-06 08:59:55 -0300

    Ajustar formato de fechas

--- OnboardingPlayground/f203281.sql
+++ OnboardingPlayground/f203281.sql
@@ -33,7 +33,7 @@ prompt APPLICATION 203281 - OnboardingPlayground
 -- Application Export:
 --   Application:     203281
 --   Name:            OnboardingPlayground
---   Date and Time:   11:24 Wednesday March 6, 2024
+--   Date and Time:   11:58 Wednesday March 6, 2024
 --   Exported By:     JAVIER.MEZA.PY@GMAIL.COM
 --   Flashback:       0
 --   Export Type:     Application Export
@@ -98,9 +98,9 @@ wwv_imp_workspace.create_flow(
 ,p_flow_language=>'es-py'
 ,p_flow_language_derived_from=>'FLOW_PRIMARY_LANGUAGE'
 ,p_allow_feedback_yn=>'Y'
-,p_date_format=>'DS'
-,p_timestamp_format=>'DS'
-,p_timestamp_tz_format=>'DS'
+,p_date_format=>'DD-MON-YYYY HH24:MI:SS'
+,p_timestamp_format=>'DD-MON-YYYY HH24:MI:SS'
+,p_timestamp_tz_format=>'DD-MON-YYYY HH24:MI:SS'
 ,p_direction_right_to_left=>'N'
 ,p_flow_image_prefix => nvl(wwv_flow_application_install.get_image_prefix,'')
 ,p_authentication_id=>wwv_flow_imp.id(70058149042696571642)
@@ -123,7 +123,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'OnboardingPlayground'
 ,p_last_updated_by=>'JAVIER.MEZA.PY@GMAIL.COM'
-,p_last_upd_yyyymmddhh24miss=>'20240306111710'
+,p_last_upd_yyyymmddhh24miss=>'20240306115701'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>6
 ,p_print_server_type=>'INSTANCE'
